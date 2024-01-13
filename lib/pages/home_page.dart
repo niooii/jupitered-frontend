5027979bc33b69a2af63321f8128e07953fdfb60
niooii <wangh3@bxscience.edu>
2024-01-12 22:57:12 -0500

    a

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,13 @@
+// this should just be a page of statistics tbh
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
