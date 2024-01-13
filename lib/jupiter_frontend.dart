f71d9126acc7e9b07e40dffed772222d550e4409
AstralTurtle <jamesacs329@gmail.com>
2024-01-13 17:12:17 -0500

    incredibly cursed dynamic theme code

--- lib/jupiter_frontend.dart
+++ lib/jupiter_frontend.dart
@@ -1,23 +1,56 @@
 import 'package:flutter/material.dart';
 import 'package:jupiter_frontend/pages/login_screen.dart';
+import 'package:catppuccin_flutter/catppuccin_flutter.dart';
 
-// test import
 import 'package:jupiter_frontend/pages/theme_page.dart';
 
-class JupiterFrontendApp extends StatelessWidget {
+// test import
+
+class JupiterFrontendApp extends StatefulWidget {
   const JupiterFrontendApp({super.key});
+
+  @override
+  State<JupiterFrontendApp> createState() => _JupiterFrontendAppState();
+
+  static getState() {
+    return _JupiterFrontendAppState.instance;
+  }
+}
+
+class _JupiterFrontendAppState extends State<JupiterFrontendApp> {
+  static late _JupiterFrontendAppState instance;
+
+  ThemeData _theme = ThemeData(
+    colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    useMaterial3: true,
+  );
+
+  @override
+  void initState() {
+    instance = this;
+    super.initState();
+  }
+
+  static void setAppTheme(ThemeData theme) {
+    instance.setTheme(theme);
+  }
+
+  void setTheme(ThemeData theme) {
+    setState(() {
+      _theme = theme;
+    });
+  }
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: //ThemePage()
-      LoginScreen(key: key,),
+      theme: _theme,
+      home: ThemePage(),
+      //     LoginScreen(
+      //   key: key,
+      // ),
     );
   }
 }
--- lib/pages/theme_page.dart
+++ lib/pages/theme_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 import "package:flutter_hsvcolor_picker/flutter_hsvcolor_picker.dart";
 
+import 'package:jupiter_frontend/jupiter_frontend.dart' as app;
+
 class ThemePage extends StatefulWidget {
   const ThemePage({super.key});
   static ThemeData getTheme(BuildContext context) {
@@ -13,17 +15,60 @@ class ThemePage extends StatefulWidget {
 }
 
 class _ThemePageState extends State<ThemePage> {
-  @override
+  List<ColorTile> tiles = [];
+
+  ThemeData newTheme(List<ColorTile> tiles) {
+    return ThemeData(
+      primaryColor: tiles[0].color,
+      colorScheme: ColorScheme(
+        primary: tiles[0].color,
+        onPrimary: tiles[1].color,
+        secondary: tiles[2].color,
+        onSecondary: tiles[3].color,
+        background: tiles[4].color,
+        onBackground: tiles[5].color,
+        surface: tiles[6].color,
+        onSurface: tiles[7].color,
+        brightness: Brightness.light,
+        error: Colors.red,
+        onError: Colors.white,
+      ),
+      useMaterial3: true,
+    );
+  }
+
   Widget build(BuildContext context) {
-    ThemeData theme = ThemePage.getTheme(context);
-    print(theme.toString());
+    ColorScheme colorScheme = Theme.of(context).colorScheme;
+    Map<String, Color> schemeColors = {
+      "primary": colorScheme.primary,
+      "onPrimary": colorScheme.onPrimary,
+      "secondary": colorScheme.secondary,
+      "onSecondary": colorScheme.onSecondary,
+      "background": colorScheme.background,
+      "onBackground": colorScheme.onBackground,
+      "surface": colorScheme.surface,
+      "onSurface": colorScheme.onSurface,
+    };
+    List<ColorTile> tiles =
+        schemeColors.entries.map((e) => ColorTile(e.value, e.key)).toList();
+
     return SafeArea(
         child: Scaffold(
+            appBar: AppBar(
+              title: const Text("Change the theme"),
+              backgroundColor: Theme.of(context).colorScheme.primary,
+            ),
             body: Center(
                 child: ListView(
-
-      children: [ColorTile(Theme.of(context).primaryColor, "primary")],
-    ))));
+              children: [...tiles],
+            )),
+            floatingActionButton: FloatingActionButton(
+              onPressed: () {
+                app.JupiterFrontendApp.getState().setTheme(newTheme(tiles));
+                // Navigator.pop(context);
+              },
+              child: Icon(Icons.check),
+            )));
   }
 }
 
@@ -37,22 +82,24 @@ class ColorTile extends StatefulWidget {
 }
 
 class _ColorTileState extends State<ColorTile> {
-
   _showDialog(BuildContext context) {
     showDialog(
         context: context,
         builder: (context) {
           return AlertDialog(
             title: Text("Pick a color"),
-            content: ColorPicker(
-              color: widget.color,
-              onChanged: (value) {
-                widget.color = value;
-                setState(() {
-                  
-                });
-              },
-              initialPicker: Picker.paletteHue,
+            content: SingleChildScrollView(
+              child: Container(
+                  height: 400,
+                  width: 400,
+                  child: ColorPicker(
+                    color: widget.color,
+                    onChanged: (value) {
+                      widget.color = value;
+                      setState(() {});
+                    },
+                    initialPicker: Picker.wheel,
+                  )),
             ),
             actions: [
               TextButton(
@@ -65,18 +112,24 @@ class _ColorTileState extends State<ColorTile> {
         });
   }
 
-
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      leading: GestureDetector(child:
-       Container(
-        width: 30,
-        height: 30,
-        color: widget.color,
-      ), onTap: () {
-        _showDialog(context);
-      },),
+      leading: GestureDetector(
+        child: Container(
+          width: 30,
+          height: 30,
+          decoration: BoxDecoration(
+            color: widget.color,
+            borderRadius: BorderRadius.circular(5),
+            border: Border.all(
+                color: Colors.grey, width: 1.75, style: BorderStyle.solid),
+          ),
+        ),
+        onTap: () {
+          _showDialog(context);
+        },
+      ),
       title: Text(widget.name),
     );
   }
