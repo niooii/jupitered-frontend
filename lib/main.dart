54b069f0ddaeecb4ed05661088a71bd1b296e141
niooii <wangh3@bxscience.edu>
2024-01-17 09:34:10 -0500

    shared prefs

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:jupiter_frontend/jupiter_frontend.dart';
 import 'package:jupiter_frontend/services/db_manager.dart';
+import 'package:jupiter_frontend/services/shared_preferences.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await CSharedPrefs().init();
   await CDbManager.getInstance().initDB();
 
   runApp(const JupiterFrontendApp());
--- lib/services/shared_preferences.dart
+++ lib/services/shared_preferences.dart
@@ -1,17 +1,24 @@
+import 'package:shared_preferences/shared_preferences.dart';
 // TODO!
 // store settings here !
 
-// Try to fetch data as little as possible. I/O operation are expensive AS FUCK
-
-
 class CSharedPrefs {
+  late SharedPreferences prefs;
   static CSharedPrefs? _instance;
 
   CSharedPrefs._();
 
+  Future<void> init() async {
+    prefs = await SharedPreferences.getInstance();
+  }
+
   factory CSharedPrefs() {
     _instance ??= CSharedPrefs._();
     return _instance!;
+  }
+
+  void save() {
+    
   }
 
 }
